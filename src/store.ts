161c8e63a2a07eb697256fa84b45c68b16c3df0a
Anderson Nunes <anderson.nunes1988@gmail.com>
2021-07-05 21:48:58 -0300

    ⚡️ Step 5: Reactive Store integration

--- src/store.ts
+++ src/store.ts
@@ -1,11 +1,16 @@
 import { BehaviorSubject, Observable } from "rxjs";
 import { distinctUntilChanged, pluck } from "rxjs/operators";
 
+import { User } from "./auth/shared/services/auth/auth.service";
+
 export type State = {
+  user: User | null
   [key: string]: any
 }
 
-const state: State = {};
+const state: State = {
+  user: null
+};
 
 export class Store {
   private subject = new BehaviorSubject(state);
